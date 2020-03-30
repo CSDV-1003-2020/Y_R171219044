dbcc4f8f1b5dd949971a40bcee40e0aae9ccef17
Yashvishnoi <jivishnoi@gmail.com>
2020-03-31 01:48:34 +0530

    calculator for addition

--- calculator.cpp
+++ calculator.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 using namespace std;
 
+int addition(int x,int y)
+{
 
+}
 int main()
 {
 int a,b,c;
 cout<<"Eneter the number "<<"\n";
-cin>>a,b;
+cin>>a>>b;
 c=addition(a,b);
 cout<<c<<endl;
 return 0;
