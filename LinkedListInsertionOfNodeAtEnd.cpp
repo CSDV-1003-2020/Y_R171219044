ecf623a38e34cfd9932e37af3dbfc27a825ff2cb
Yashvishnoi <jivishnoi@gmail.com>
2020-03-31 01:39:59 +0530

    Insertion of node at end

---
+++ LinkedListInsertionOfNodeAtEnd.cpp
@@ -0,0 +1,48 @@
+
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+Node* head = NULL; 
+
+void Insert(int x)
+{
+   Node* newnode = new Node;
+   newnode->data=x;
+   newnode->next=NULL;
+
+   if(head == NULL)
+   {
+        head=newnode;
+   }
+   else
+   {
+        Node* last=head;
+        while(last->next != NULL) last=last->next;
+        last->next = newnode;
+   }
+}
+	
+
+
+int main()
+{
+	int n,x,i;
+	cout<<"Enter the value how much node you want to enter"<<"\n";
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<" Enter the number to be entered ";
+		cin>>x;
+		Insert(x);
+	}
+    Print();
+    return 0;
+}
+
+
