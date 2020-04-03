ac62fd0d462cb4b9472e1672cef604adbc26fade
Vijay1435 <500077690@stu.upes.ac.in>
2020-04-03 10:34:40 +0530

    change in program

--- calculator1.cpp
+++ calculator1.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 int add(int a,int b)
 {
-int c=a+b;
-return c;
+int s=a+b;
+return s;
 }
 
 int main()
 {
-int a,b,c;
-c=add(a,b);
-cout<<c;
+int a,b,s;
+s=add(a,b);
+cout<<s;
 return 0;
 }
